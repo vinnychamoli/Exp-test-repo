486c696bb07a2d48879359246cc9f4ca4be91c0f
vinnychamoli <61835831+vinnychamoli@users.noreply.github.com>
2020-04-05 17:47:36 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Hello
+{
+
+      public static void main(String args[])
+      {
+            for(int i=1; i<=10;i++)
+               {
+                  System.out.println("hello");
+                 }
+        }
+}
+       
